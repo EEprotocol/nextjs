577142f177b49b958d37818eab6c49f1a6358da5
EEprotocol <kei310c@outlook.com>
2025-11-19 08:49:47 +0900

    cont0 dependence

---
+++ app/craftlog/article/cont0/page.tsx
@@ -0,0 +1,17 @@
+import Footer from "@/app/Footer";
+import Header from "@/app/Header";
+import "@/app/App.css";
+import "math";
+import "@/styles/craftlog.css";
+import Content from "./page.mdx";
+
+export default function Page() {
+  return (
+    <>
+    <Header>CRAFT LOG</Header>
+    <Content />
+    <Footer />
+    </>
+
+  )
+}
