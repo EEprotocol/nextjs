da29e28616296becd3c922f96e82ddb568b2f88b
EEprotocol <kei310c@outlook.com>
2025-11-19 09:14:37 +0900

    local dev success

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,14 @@ const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+   output:'export'
 
 }
  
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-  //extension: /\.(md|mdx)$/,
-  options:{
+  extension: /\.(md|mdx)$/,
+  option:{
     remarkPlugins:[remarkGfm],
   },
 })
