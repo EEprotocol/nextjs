d6d03e7409c0bb337d9a2c11ecb9dca6266f0766
EEprotocol <kei310c@outlook.com>
2025-12-08 22:46:59 +0900

    [modify]

--- app/Header.tsx
+++ app/Header.tsx
@@ -14,6 +14,7 @@ function Header({ children }: { children: React.ReactNode }) {
       if (children === "CRAFT LOG") setPath("/craftlog");
       else if (children === "ABOUT") setPath("/about");
       else if (children === "NEWS") setPath("/news");
+      else if (children === "LINK") setPath("/link");
     }
   }, [children]);
 
